4ae836f207915f1ebdd2c0b1dd41a560c940efd0
Youngjo Jang <youngjo.jang@gmail.com>
2022-08-04 21:20:38 +0900

    project setup

---
+++ src/main/java/io/foresight/websocket/SpringNettyWebsocketApplication.java
@@ -0,0 +1,13 @@
+package io.foresight.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNettyWebsocketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringNettyWebsocketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/foresight/websocket/SpringNettyWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.foresight.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNettyWebsocketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
